1392db27c3370955900587b26f08c8a28b3b1021
ayaelshair <elshairaya@gmail.com>
2026-01-20 22:31:14 +0300

    Refactor DB connection to use DATABASE_URL

--- config/db.js
+++ config/db.js
@@ -6,11 +6,10 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD), 
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("connect", () => {
