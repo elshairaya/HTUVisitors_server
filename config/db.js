0b893e1f51a88f8af50fe49d53e4ef7a36210372
elshairaya <elshairaya@gmail.com>
2026-01-20 22:28:20 +0300

    Final Updates

--- config/db.js
+++ config/db.js
@@ -6,11 +6,8 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD), 
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+   connectionString: process.env.DATABASE_URL,
+
 });
 
 pool.on("connect", () => {
